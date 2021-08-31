ccdfdedd40ab7fb59ef8433b9d9cd2f5661c7dba
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-31 11:19:36 +0100

    Create alltherest.cs

---
+++ alltherest.cs
@@ -0,0 +1,13 @@
+compileXCurrentGithubPage(Sdfgtredffghgadssfcvbuseridfghnbvgfbbbbvve45tgyyggihnnjbbbbbhhhgfdr4ttyy6yyttrr/scmyttgbbbbbadgetonfirstbadluckgetmy7seventycomhjgdsafvbvbvbnotypefourofourfd, MiddleMadScript)
+Part 'Run' {
+onevent Click "Run",OnfIlemenyu {
+runGITHUBSCCOLORCODEDSYNTAXSLASHGETSONGOFIDSYNTAXGETRUNONSMALLPAGEGIVE"BACK"CHOICEADDGETSTRUCKLIGHTANDGETFINDFG"JKLKLHGgItgbhygd4wq9p0hj73sa7b3xz:poyuiascdfghiuserdsafhjh:fgha2.jpg"
+}
+AddGetWindowAsdSfGHhBelowIdFGHhhgroUtEWrnHGHgNhHJHjJhgGNBv
+Asclop(5)
+Addd Revent Saveasp
+}
+Part 'Save {
+wflsa add OpenFileExplorerSaveAsWindowSAC09IHHBBGGHBHGFHHHY7T5T5REERERGY66TTU9IIII9U8UYYYTDFDFVVCFCVXSZSDFGBB ANSWERVAREQU=FileExplorerSaveAs.name SaveAs ANSWERVAREQU + ".intplge"
+}
+Bracketscgf intslise
