b9697a77bbe38fd73a74639cf563e12d68dbf4bf
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-31 10:59:51 +0100

    Create tweak.cs

---
+++ tweak.cs
@@ -0,0 +1,16 @@
+using Didder ONFOURTSZ;
+using Tweakers CSer;
+using Hello Me;
+using Twes;
+using Siu Big;
+using Display Dash;
+using OnEvfg Config;
+using Strdexe;
+gcreate 'Dash'
+// If
+[Gcreate]get r-jet-fusion-rocket[/G] Using.y
+Create lar;
+[GStateIntProgrammingLanguage][OnLeftDownCornerIntGetHere]"Int Programming Language"
+                                                          "Version 11"
+[/AllStatementStrEnd]
+topmenu fileOnlyOkMenButton _LEFT INSTRUCT "Save" "Run"
